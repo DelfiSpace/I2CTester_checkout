a19787d6170ee25242ad3b1e29625c7a1ae39387
Stefano Speretta <s.speretta@tudelft.nl>
2016-06-14 11:20:34 +0200

    Added LGPL license

---
+++ README.md
@@ -0,0 +1,2 @@
+# I2CTester_checkout
+This is an application designed to test the functionality of he I2C Tester setup. The setuo includes several devices on different boards to perform a long duration test on the hardware and software implementation. It is designed to use Energia (the Arduino port for MSP microcontrollers) on an MSP432.
--- tests.h
+++ tests.h
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2016 by Stefano Speretta <s.speretta@tudelft.nl>
+ *
+ * I2CTester_checkout: this is an application designed to test the 
+ * functionality of he I2C Tester setup. The setuo includes several 
+ * devices on different boards to perform a long duration test on 
+ * the hardware and software implementation. It is designed to use 
+ * Energia (the Arduino port for MSP microcontrollers) on an MSP432.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License 
+ * version 3, both as published by the Free Software Foundation.
+ *
+ */
+ 
 #ifndef __TESTS_H__
 #define __TESTS_H__
 
